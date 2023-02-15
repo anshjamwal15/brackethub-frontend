c1febfaceb7c657ba17ce8441f05d8f58500560c
aimbot1526 <anshjaamwal2002@gmail.com>
2023-02-16 03:04:12 +0530

    new-ignore

--- app/src/main/java/com/example/brackethub_frontend/MainActivity.kt
+++ app/src/main/java/com/example/brackethub_frontend/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             BrackethubfrontendTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("binod")
+                    Greeting("welcome")
                 }
             }
         }
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello, $name!")
 }
 
 @Preview(showBackground = true)
