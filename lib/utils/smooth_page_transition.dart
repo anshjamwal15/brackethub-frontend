b8658af50135b3c2e074c8e4e7e78b36ed1cc6eb
aimbot1526 <anshjaamwal2002@gmail.com>
2023-07-05 07:19:20 +0530

    fix: page transition fix

--- lib/utils/smooth_page_transition.dart
+++ lib/utils/smooth_page_transition.dart
@@ -1,28 +1,25 @@
 import 'package:flutter/material.dart';
 
-void smoothNavigate(BuildContext context, Widget page, {bool? pop}) {
-  Future.delayed(const Duration(milliseconds: 500), () {
-    Navigator.push(
-      context,
-      PageRouteBuilder(
-        transitionDuration: const Duration(milliseconds: 800),
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          var begin = const Offset(1.0, 0.0);
-          var end = Offset.zero;
-          var curve = Curves.ease;
+const begin = Offset(1.0, 0.0);
+const end = Offset.zero;
+const curve = Curves.ease;
 
-          var tween =
-              Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
-          return SlideTransition(
-            position: animation.drive(tween),
-            child: child,
-          );
-        },
-        pageBuilder: (context, animation, secondaryAnimation) {
-          return page;
-        },
-      ),
-    );
-  });
+void smoothNavigate(BuildContext context, Widget page) {
+  Navigator.push(
+    context,
+    PageRouteBuilder(
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+      pageBuilder: (context, animation, secondaryAnimation) {
+        return page;
+      },
+    ),
+  );
 }
