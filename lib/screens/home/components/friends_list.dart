2508d26d4ba04622f1710196eca40cea66c4285d
aimbot1526 <anshjaamwal2002@gmail.com>
2023-07-26 00:58:16 +0530

    build: scrollable complete

--- lib/screens/home/components/friends_list.dart
+++ lib/screens/home/components/friends_list.dart
@@ -1,7 +1,5 @@
 import 'package:brackethub_app/utils/custom_input_field.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-
 import '../../../utils/utils.dart';
 import '../../../widgets/user_status_container.dart';
 
@@ -11,37 +9,20 @@ class FriendsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 10, top: 40),
-                child: CustomText(
-                  text: "Direct Messages",
-                  fontSize: 20,
-                  weight: FontWeight.bold,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 10, top: 10, right: 20),
-                child: SizedBox(
-                  child: CustomInputField(
-                    hintText: "Find or start a conversation",
-                  ),
-                ),
-              )
-            ],
-          ),
-          ListView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: 2,
-            itemBuilder: (context, index) {
+    //FIXME: last item is not visible in list
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          backgroundColor: kBackgroundColor,
+          automaticallyImplyLeading: false,
+          expandedHeight: 130,
+          pinned: true,
+          flexibleSpace: _buildSliverAppBarContent(),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 25,
+            (context, index) {
               return SizedBox(
                 height: size.height * 0.08,
                 child: Padding(
@@ -65,9 +46,44 @@ class FriendsList extends StatelessWidget {
                 ),
               );
             },
-          )
-        ],
-      ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSliverAppBarContent() {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Container(
+          color: kBackgroundColor,
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                FlexibleSpaceBar(
+                  centerTitle: false,
+                  titlePadding: EdgeInsets.only(left: 12, bottom: 70),
+                  title: CustomText(
+                    text: "Direct Messages",
+                    fontSize: 18,
+                    weight: FontWeight.w500,
+                  ),
+                ),
+                Positioned(
+                  left: 12,
+                  right: 20,
+                  bottom: -2,
+                  child: CustomInputField(
+                    hintText: "Find or start a conversation",
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
